dba8c405ded02b2b86571e914b20bca18eea5cbe
PinoMacri <pinonegozio1@outlook.it>
2022-12-19 00:10:40 +0100

    BONUS tutti consegnati

--- js/script.js
+++ js/script.js
@@ -4,39 +4,78 @@
 3) - Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 4) - Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro,
 ed emetto un messaggio in console con il numero della cella cliccata.
+5)  NO BOOTSTRAP, RIPASSO CSS A MANO
 */
-//! NO BOOTSTRAP, RIPASSO CSS A MANO
-//^ MILESTONE 1
+//* MILESTONE 1
 // Prepariamo l'HTML ed il CSS per ottenere il risultato grafico che vediamo nell'immagine allegata.
-//^MILESTONE 2
+//*MILESTONE 2
 /* Rimuoviamo le celle che abbiamo inserito nell'HTML in modo da generarle tramite JS. 
 Al click del bottone play, vengono generate 100 celle in 10 righe da 10 celle ciascuna. */
-//^ MILESTONE 3
+//* MILESTONE 3
 // In ogni cella, deve comparire il numero corrispondente, in ordine da 1 a 100;
-//^ MILESTONE 4
+//* MILESTONE 4
 // Al click sulla cella, stampiamo il numero della cella cliccata in console, poi coloriamo la cella d'azzurro!
-
-
+//! ---------------------------------------SVOLGIMENTO-----------------------------------------
 const realGrid = document.getElementById("realGrid")
+//^ --------------DIFFICOLTA' 1-------------------
 const button = document.getElementById("button")
-button.addEventListener ("click" , function () {
-    realGrid.classList.add ("active")
+button.addEventListener("click", function () {
+    realGrid.classList.add("active")
+    difficileDue.classList.remove("active")
+    difficileTre.classList.remove("active")
 }
 )
 for (i = 1; i <= 100; i++) {
     const realGridChild = document.createElement("div")
-    realGridChild.classList.add("cell" , "flex" , "center" , "styleCell")
+    realGridChild.classList.add("cell", "flex", "center", "styleCell")
     const finalGenerate = realGrid.appendChild(realGridChild)
     finalGenerate.innerText = i;
     let finalGenerateClick;
     finalGenerate.addEventListener("click", function () {
-        finalGenerateClick = realGridChild.classList.add("azzurro" )
+        finalGenerateClick = realGridChild.classList.add("azzurro")
         console.log(finalGenerate.innerText)
     })
 }
-
-
-
+//^ --------------DIFFICOLTA' 2-------------------
+const buttonDue = document.getElementById("buttonDue")
+const difficileDue = document.getElementById("difficileDue")
+buttonDue.addEventListener("click", function () {
+    difficileDue.classList.add("active")
+    realGrid.classList.remove("active")
+    difficileTre.classList.remove("active")
+}
+)
+for (i = 1; i <= 81; i++) {
+    const realGridDue = document.createElement("div")
+    realGridDue.classList.add("realGridDue", "flex", "center", "styleCell")
+    const finalGenerateDue = difficileDue.appendChild(realGridDue)
+    finalGenerateDue.innerText = i;
+    let finalGenerateClickDue;
+    finalGenerateDue.addEventListener("click", function () {
+        finalGenerateClickDue = realGridDue.classList.add("azzurro")
+        console.log(finalGenerateDue.innerText)
+    })
+}
+//^ --------------DIFFICOLTA' 3--------------------
+const buttonTre = document.getElementById("buttonTre")
+const difficileTre = document.getElementById("difficileTre")
+buttonTre.addEventListener("click", function () {
+    difficileTre.classList.add("active")
+    realGrid.classList.remove("active")
+    difficileDue.classList.remove("active")
+}
+)
+for (i = 1; i <= 49; i++) {
+    const realGridTre = document.createElement("div")
+    realGridTre.classList.add("realGridTre", "flex", "center", "styleCell")
+    const finalGenerateTre = difficileTre.appendChild(realGridTre)
+    finalGenerateTre.innerText = i;
+    let finalGenerateClickTre;
+    finalGenerateTre.addEventListener("click", function () {
+        finalGenerateClickTre = realGridTre.classList.add("azzurro")
+        console.log(finalGenerateTre.innerText)
+    })
+}
 
 
 
